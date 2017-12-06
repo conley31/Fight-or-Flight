b170fb83273780dafacc53b65391206b6d276fd3
U-BRANDON-ASUS\Brandon <kent13@purdue.edu>
2017-12-05 20:30:11 -0500

    added new enemy type

--- public/enemy.js
+++ public/enemy.js
@@ -18,6 +18,53 @@ function wideEnemy(x, y) {
 	this.enemyID = numOfEnemies;
 	numOfEnemies++;
 }
+
+function fastEnemy(x, y) {
+	this.x = x;
+	this.y = y;
+	this.hp = 1;
+	this.width = 25;
+	this.height = 55;
+	this.enemyID = numOfEnemies;
+	numOfEnemies++;
+}
+
+fastEnemy.prototype = {
+	width: 25,
+	height: 55,
+	draw: function() {
+		this.y = this.y + 2.5;
+		if (this.hp == 0) {
+			delete enemies[this.enemyID];
+			return;
+		}
+		if (this.y > h) {
+			gameState = "GAME_OVER";
+		}
+		var posX = this.x;
+		var posY = this.y;
+		ctx.fillStyle = "#FF0000";
+		ctx.fillRect(posX, posY, this.width, this.height);
+	},
+	collision: function() {
+		for (i = 0; i < numOfProjectiles; i++) {
+			if (projectiles[i] != null) {
+				if (projectiles[i].x > this.x && projectiles[i].x < (this.x + this.width) && projectiles[i].y < this.y && projectiles[i].y > (this.y - this.height)) {
+					players[projectiles[i].playerID].score += 10;
+					this.hp--;
+					if (this.hp == 0) {
+						players[projectiles[i].playerID].score += 100;
+					}
+					delete projectiles[i];
+				}
+			}
+		}
+	}
+
+
+}
+
+
 wideEnemy.prototype = {
 	width: 70,
 	height: 30,
